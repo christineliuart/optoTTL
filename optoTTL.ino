bbd3d52a55a1b110294554c304150a0021fd13c6
christineliuart <70307976+christineliuart@users.noreply.github.com>
2024-01-31 20:09:46 -0800

    Update optoTTL.ino

--- optoTTL.ino
+++ optoTTL.ino
@@ -8,6 +8,7 @@
 #define BURST 2       //button
 #define TONIC 3       //button
 #define STOP 4        //button
+#define LED A4        // optional LED with long lead connected to resistor, connected to A4, and short lead connected to ground
 
 // STIM PARAMETERS
 int tonic_pulse_width = 4;            //ms
@@ -25,38 +26,50 @@ void setup() {
   pinMode(TONIC, INPUT);
   pinMode(STOP, INPUT);
   pinMode(PULSE_OUT,OUTPUT);
+  pinMode(LED, OUTPUT);
 
   digitalWrite(PULSE_OUT,LOW); //ensures TTL pulse is set to LOW so laser is off before pulses are sent
+  digitalWrite(LED_BUILTIN,LOW);
+  Serial.begin(9600);
 }
 
 void loop() {
 
-  if (BURST == HIGH) {
+  if (digitalRead(BURST) == HIGH) {
     for (int cycles = 0; cycles < phasic_burst_cycles; cycles++) {
+      digitalWrite(LED,HIGH);
       digitalWrite(PULSE_OUT,HIGH);
       delay(phasic_pulse_width);
       digitalWrite(PULSE_OUT,LOW);
+      digitalWrite(LED,LOW);
       long phasic_temp_delay = (1000 / phasic_frequency) - phasic_pulse_width;
       if (phasic_temp_delay < 0) phasic_temp_delay = 0;
       delay(phasic_temp_delay);
     }
+    Serial.println("BURST");
   }
 
-  if (TONIC == HIGH) {
+  if (digitalRead(TONIC) == HIGH) {
     tonic = 1; //turn on tonic stim
+    Serial.println("TONIC");
   }
 
-  if (tonic == 1) {
+  while (tonic == 1) {
+    digitalWrite(LED,HIGH);
     digitalWrite(PULSE_OUT, HIGH);
     delay(tonic_pulse_width);
     digitalWrite(PULSE_OUT,LOW);
+    digitalWrite(LED,LOW);
     long tonic_temp_delay = (1000 / tonic_frequency) - tonic_pulse_width;
     if (tonic_temp_delay < 0) tonic_temp_delay = 0;  //avoid crashing
-    delay(tonic_temp_delay);     
+    delay(tonic_temp_delay);    
+
+    if (digitalRead(STOP) == HIGH) break; 
   }
 
-  if (STOP == HIGH) {
+  if (digitalRead(STOP) == HIGH) {
     tonic = 0; //turn off tonic stim
+    Serial.println("STOP");
   }
 
 }
